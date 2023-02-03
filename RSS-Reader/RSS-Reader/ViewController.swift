8810f1a680541f8a0fc2d3b5ad350989c0102043
Artem Oleshkevich <oleshkevich.artem.1998@gmail.com>
2023-02-03 11:27:00 +0300

    Created a new project file RSS-Reader

---
+++ RSS-Reader/RSS-Reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSS-Reader
+//
+//  Created by Oleshkevich Artem on 3.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
